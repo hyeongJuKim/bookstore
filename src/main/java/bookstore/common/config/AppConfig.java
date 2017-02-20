cad9b19e98a232f12f562725834cd54f3dec8d0b
HJ <hj@HJui-MacBook-Pro.local>
2017-02-21 00:00:12 +0900

    commit

---
+++ src/main/java/bookstore/common/config/AppConfig.java
@@ -0,0 +1,8 @@
+package bookstore.common.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+	//TODO: 데이터베이스 접속, 트랜잭션 관리, DAO, service를 정의하는 부분.
+}
---
+++ src/main/java/bookstore/rest/RestConfig.java
@@ -0,0 +1,5 @@
+package bookstore.rest;
+
+public class RestConfig {
+
+}
